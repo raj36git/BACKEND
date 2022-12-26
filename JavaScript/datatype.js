d7c3c7185756052aa6ee95df2aeaab812967b9d2
Raj Yadav <108160409+raj36git@users.noreply.github.com>
2022-12-27 03:34:39 +0545

    datatype code is added

---
+++ JavaScript/datatype.js
@@ -0,0 +1,33 @@
+//Number
+var num1 = 44;
+
+//Strng
+var name  = "Razz";
+var surname = "Yadav";
+
+//boolean 
+var isPassesd = true;
+var isTeen =  false;
+
+//Objects
+var user = {
+    Name :"Razz",
+    Degree : "B-tech",
+    Company : {
+            first : "Amazon",
+            second : "Uber",
+            third : "Microsoft",
+            presenet : "Google"
+            } 
+}
+
+//null
+var result = null;
+
+console.log(num1);
+console.log(name);
+console.log(surname);
+console.log(isPassesd);
+console.log(isTeen);
+console.log(user);
+console.log(result);
